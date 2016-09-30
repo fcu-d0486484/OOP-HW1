b20f9a06ea8bd51a298e52471055af2ba934011f
user <user@FE08>
2016-09-30 10:03:48 +0800

    FinalWork Ver2.5

--- src/fcu/iecs/opp/java/Hello.java
+++ src/fcu/iecs/opp/java/Hello.java
@@ -4,6 +4,10 @@ public class Hello {
 	public static void main(String[] args)
 	{
 		String lyrics = new String("Let it go!Let it go! Cannot hold it back anymore");
+		
+		lyrics = lyrics.replace("it","her");
+		lyrics = lyrics.replace("Cannot","Can't");
 		System.out.println(lyrics);
+		
 	}
 }
