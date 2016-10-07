561dedeba662803fb9954fa55bb55fe4567a05a8
user <user@FE08>
2016-10-07 08:24:16 +0800

    FinalWork Ver2.6 change out line

--- src/fcu/iecs/oop/java/Hello.java
+++ src/fcu/iecs/oop/java/Hello.java
@@ -4,10 +4,9 @@ public class Hello {
 	public static void main(String[] args)
 	{
 		String lyrics = new String("Let it go!Let it go! Cannot hold it back anymore");
-		
+		System.out.println("更動前:"+lyrics);
 		lyrics = lyrics.replace("it","her");
 		lyrics = lyrics.replace("Cannot","Can't");
-		System.out.println(lyrics);
-		
+		System.out.println("更動後:"+lyrics);
 	}
 }
